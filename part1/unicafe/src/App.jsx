2453ccc21eac4d9a285dc25065b7f1222642d296
Ivan Khimich <himivan@gmail.com>
2023-12-17 23:45:30 +0100

    exercise 1.6: add Feedback component with child components

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,14 +1,29 @@
 import { useState } from 'react'
 
+const Button = ({text, handleClick}) => {
+  return <button onClick={handleClick}>{text}</button>
+}
+
+const Feedback = ({handleGood, handleNeutral, handleBad}) => {
+  return <div>
+    <h1>give feedback</h1>
+    <Button text='good' handleClick={handleGood} />
+    <Button text='neutral' handleClick={handleNeutral} />
+    <Button text='bad' handleClick={handleBad} />
+  </div>
+}
+
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
   return (
     <div>
-      code here
+      <Feedback handleGood={() => setGood(good + 1)}
+                handleNeutral={() => setNeutral(neutral + 1)}
+                handleBad={() => setBad(bad + 1)} 
+      />
     </div>
   )
 }
