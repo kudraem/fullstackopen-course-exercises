05ada3a3fa35db737142088e2245efe61879c660
Ivan Khimich <himivan@gmail.com>
2023-12-20 20:56:59 +0100

    exercise 2.1: add "Course" and required child components

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,3 +1,38 @@
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header text={course.name} />
+      <Content parts={course.parts}/>
+    </div>
+  )
+}
+
+const Header = ({text}) => {
+  return (
+    <>
+      <h1>{text}</h1>
+    </>
+  )
+}
+
+const Content = ({parts}) => {
+  return (
+    <>
+      {parts.map(part => 
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      )}
+    </>
+  )
+}
+
+const Part = ({name, exercises}) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  )
+}
+
 const App = () => {
   const course = {
     id: 1,
@@ -12,16 +47,13 @@ const App = () => {
         name: 'Using props to pass data',
         exercises: 7,
         id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
       }
     ]
   }
 
-  return <Course course={course} />
+  return (
+    <Course course={course}/>
+  )
 }
 
 export default App
